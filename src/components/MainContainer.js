a7384a1941790ca1c139da0b345fcb738e3f57d7
minjeongHEO <hmjoung9770@gmail.com>
2023-09-17 02:47:16 +0900

    useEffect사용하여 사라지는 팝업 만들기, 링크걸기

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 const MainContainer = (props) => {
   return (
@@ -10,12 +11,13 @@ const MainContainer = (props) => {
       <Row>
         {props.shoes.map((data) => (
           <Col>
-            <img src={data.img} width="80%" />
-            <h4>{data.title}</h4>
-            <p>{data.price}</p>
+            <Link to={`/detail/${data.id}`}>
+              <img src={data.img} width="80%" />
+              <h4>{data.title}</h4>
+              <p>{data.price}</p>
+            </Link>
           </Col>
         ))}
-
         {/* <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%" /> */}
       </Row>
     </Container>
--- src/components/page/DetailPage.js
+++ src/components/page/DetailPage.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import styles from './DetailPage.module.css';
+import { cleanup } from '@testing-library/react';
 
 let ColorBtn = styled.button`
   background: ${(props) => props.bg};
@@ -22,11 +25,26 @@ const DetailPage = (props) => {
   // const selectedData = data.find((item) => item.id === parseInt(id));
   let filteredData = props.data.filter((item) => item.id === parseInt(id));
 
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+
+    //useEffect동작 전에 실행되는 return()=>{}
+    return () => {
+      clearTimeout(timer);
+      //기존코드 치우는 함수(ex.기존 타이머는 제거해주세요)
+      //참고. clean up function은 mount시 실행X, unmount(컴포넌트삭제)시 실행O
+    };
+  }, [isVisible]); //isVisible변수가 변할 때만 실행됨 (처음 페이지 마운트 될 때는 물론 실행됨)
+  // }, []); : 컴포넌트 마운트되고 1회만 실행시키고 싶으면 이렇게 빈값넣으면 됨
   return (
     <div className="container">
-      <BlackBox>
+      {/* <BlackBox>
         <NewColorBtn bg="pink">버튼</NewColorBtn>
-      </BlackBox>
+      </BlackBox> */}
+      <div className={`alert alert-warning ${isVisible ? '' : styles.fadeOut}`}>2초 이내 구매 시 할인</div>
       <div className="row">
         <div className="col-md-6">
           <img src={filteredData[0].img} width="100%" alt="" />
